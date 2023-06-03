f42dbe39e353900642364f21125b2fdf1f65ac1e
minatorak <hotmail22866@gmail.com>
2023-06-04 01:28:46 +0700

    explain fork to child process

--- concurrency/con.go
+++ concurrency/con.go
@@ -9,10 +9,10 @@ func someFunc(num string) {
 	fmt.Println(num)
 }
 func main() {
-	go someFunc("1")
+	go someFunc("1") // fork to child
 	go someFunc("2")
 	go someFunc("3")
 
-	time.Sleep(time.Second * 2)
 	fmt.Println("hi")
+	time.Sleep(time.Second * 2)
 }
