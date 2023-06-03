df7c1e939b4901bbfdd43cda64b756675429900d
minatorak <hotmail22866@gmail.com>
2023-06-04 01:16:10 +0700

    make someFunc to goroutine by go keyword

--- concurrency/con.go
+++ concurrency/con.go
@@ -1,16 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func someFunc(num string) {
 	fmt.Println(num)
 }
 func main() {
-	someFunc("1")
+	go someFunc("1")
+	go someFunc("2")
+	go someFunc("3")
 
 	fmt.Println("hi")
 }
-
-// result
-// 1
-// hi
