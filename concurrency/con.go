153131733dc1e233116aff2d9d5b0fd5839213a0
minatorak <hotmail22866@gmail.com>
2023-06-04 03:06:39 +0700

    buffered channel with small channel

--- concurrency/con.go
+++ concurrency/con.go
@@ -2,25 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	unbufferedChannel := make(chan string, 0)
-	chars := []string{"a", "b", "c"}
+	bufferedChannel := make(chan string, 1)
+	chars := []string{"a", "b", "c", "d", "e"}
 
 	go func() {
 		for _, c := range chars {
 			fmt.Println("goroutine sender ", c)
-			unbufferedChannel <- c
+			bufferedChannel <- c
 		}
 	}()
 
 	n := len(chars)
 	for n >= 1 {
-		time.Sleep(time.Second * 1)
+		// time.Sleep(time.Second * 1)
 		fmt.Println("start receive")
-		receive := <-unbufferedChannel
+		receive := <-bufferedChannel
 		fmt.Println("receive ", receive)
 		n -= 1
 	}
