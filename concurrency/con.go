f01125df707bf18022b97a3fe7644df3930016bc
minatorak <hotmail22866@gmail.com>
2023-06-05 01:48:58 +0700

    pipeline chain of channel, for send data async

--- concurrency/con.go
+++ concurrency/con.go
@@ -1,28 +1,43 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-func main() {
-	done := make(chan bool)
+func sliceToChannel(nums []int) <-chan int {
+	out := make(chan int)
 
-	go doWork(done)
-	time.Sleep(time.Second * 2)
-	close(done) // cancelled child goroutine
-	fmt.Println("after close done")
-	time.Sleep(time.Second * 5)
+	go func() {
+		for _, n := range nums {
+			fmt.Println("sliceToChannel ", n)
+			out <- n
+		}
+		close(out) // wait channel
+	}()
+	return out
 }
 
-func doWork(done <-chan bool) {
-	for {
-		select {
-		case <-done:
-			return
-		default:
-			fmt.Println("Doing infinite work")
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		for n := range in {
+			fmt.Println("sq ", n)
+			out <- n * n
 		}
+		close(out) // wait channel
+	}()
+	return out
+}
+
+func main() {
+
+	// input
+	nums := []int{2, 3, 4, 5, 7, 9}
+	dataChannel1 := sliceToChannel(nums)
+
+	finalChannel1 := sq(dataChannel1)
+
+	for n := range finalChannel1 {
+		fmt.Println(n)
 	}
 
 }
