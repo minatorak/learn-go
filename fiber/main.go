e917a44411b8f14aa7cb07c52233706ad6289ea8
minatorak <hotmail22866@gmail.com>
2023-06-07 00:42:36 +0700

    log request response

--- fiber/main.go
+++ fiber/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
 func main() {
 	app := fiber.New(fiber.Config{
 		Prefork: true,
 	})
+
+	app.Use(csrf.New())
+	app.Use(requestid.New())
+	app.Use(logger.New(logger.Config{
+		Format: "${pid} ${locals:requestid} ${status} - ${method} ${path} ${body} -> ${resBody} \n",
+	}))
 
 	app.Get("/:value", func(c *fiber.Ctx) error {
 		result := c.Params("value")
